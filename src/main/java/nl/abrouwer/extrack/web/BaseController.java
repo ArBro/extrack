1eb25dbd707a3a8d05928e052da4392c0969c65c
Arco Brouwer <abrouwer@flexyz.com>
2019-05-01 12:14:02 +0200

    Temporary disable websecurity for dev purposes

---
+++ src/main/java/nl/abrouwer/extrack/web/BaseController.java
@@ -0,0 +1,16 @@
+package nl.abrouwer.extrack.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class BaseController
+{
+	@GetMapping(value = "**")
+	public String underConstruction()
+	{
+		return "index";
+	}
+}
