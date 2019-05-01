e85ec761e6fbd1e5752141a8b5444c68d0927a45
Arco Brouwer <abrouwer@flexyz.com>
2019-05-01 11:51:03 +0200

    Initial commit

---
+++ src/main/java/nl/abrouwer/extrack/ExtrackApplication.java
@@ -0,0 +1,13 @@
+package nl.abrouwer.extrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtrackApplication.class, args);
+	}
+
+}
