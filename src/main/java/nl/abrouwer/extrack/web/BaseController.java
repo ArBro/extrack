f9fb4713935d79f1c784b7a604e91fc1217bff68
Arco Brouwer <abrouwer@flexyz.com>
2019-05-06 23:08:04 +0200

    Webjars added + simple under construction index page

--- src/main/java/nl/abrouwer/extrack/web/BaseController.java
+++ src/main/java/nl/abrouwer/extrack/web/BaseController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class BaseController
 {
-	@GetMapping(value = "**")
+	@GetMapping(value = "")
 	public String underConstruction()
 	{
 		return "index";
